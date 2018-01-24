c9f32b1cb3b19e2a45523db04d019499d8dee573
huangjoyce3 <jhuang3@uw.edu>
2018-01-24 13:17:46 -0800

    past second half of tests

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -10,32 +10,27 @@ import Foundation
 
 // All your work will go in here
 class Calculator {
-    //assert(calc.add(lhs: 2, rhs: 2) == 4)
-    //assert(calc.subtract(lhs: 2, rhs: 2) == 0)
-    //assert(calc.multiply(lhs: 2, rhs: 2) == 4)
-    //assert(calc.divide(lhs: 2, rhs: 2) == 1)
-    
-    func add(lhs : Int, rhs : Int) -> Int {
+    func add(lhs: Int, rhs: Int) -> Int {
         return lhs + rhs
     }
     
-    func subtract(lhs : Int, rhs : Int) -> Int {
+    func subtract(lhs: Int, rhs: Int) -> Int {
         return lhs - rhs
     }
     
-    func multiply(lhs : Int, rhs : Int) -> Int {
+    func multiply(lhs: Int, rhs: Int) -> Int {
         return lhs * rhs
     }
     
-    func divide(lhs : Int, rhs : Int) -> Int {
+    func divide(lhs: Int, rhs: Int) -> Int {
         return lhs / rhs
     }
     
-    func mathOp(lhs : Int, rhs : Int, op : (Int, Int) -> Int ) -> Int{
+    func mathOp(lhs: Int, rhs: Int, op: (Int, Int) -> Int ) -> Int{
         return op(lhs, rhs)
     }
     
-    func add(_ arr : [Int]) -> Int {
+    func add(_ arr: [Int]) -> Int {
         var sum = 0
         for num in arr {
             sum += num
@@ -43,7 +38,7 @@ class Calculator {
         return sum
     }
     
-    func multiply(_ arr : [Int]) -> Int {
+    func multiply(_ arr: [Int]) -> Int {
         var product = 1
         for num in arr {
             product *= num
@@ -51,11 +46,36 @@ class Calculator {
         return product
     }
     
-    func count(_ arr : [Int]) -> Int {
+    func count(_ arr: [Int]) -> Int {
         return arr.count
     }
     
-    func avg(_ arr : [Int]) -> Int {
+    func avg(_ arr: [Int]) -> Int {
         return add(arr) / arr.count
     }
+
+    func mathOp(args: [Int], beg: Int, op: (Int, Int) -> Int) -> Int {
+        var begin = beg
+        for num in args {
+            begin = op(num, begin)
+        }
+        return begin
+    }
+    
+    func add(lhs: (Int, Int), rhs: (Int, Int)) -> (Int,Int) {
+        return (add(lhs: lhs.0, rhs: rhs.0), add(lhs: lhs.1, rhs: rhs.1))
+    }
+    
+    func subtract(lhs: (Int, Int), rhs: (Int, Int)) -> (Int,Int) {
+        return (subtract(lhs: lhs.0, rhs: rhs.0), subtract(lhs: lhs.1, rhs: rhs.1))
+    }
+    
+    func add(lhs: [String: Int], rhs: [String: Int]) -> [String: Int] {
+        return ["x": add(lhs: lhs["x"]!, rhs: rhs["x"]!), "y": add(lhs: lhs["y"]!, rhs: rhs["y"]!)]
+    }
+    
+    func subtract(lhs: [String: Int], rhs: [String: Int]) -> [String: Int] {
+        return ["x": subtract(lhs: lhs["x"]!, rhs: rhs["x"]!), "y": subtract(lhs: lhs["y"]!, rhs: rhs["y"]!)]
+    }
+    
 }
